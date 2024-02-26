f6b26b47efca6ecd555c50cbef4d609cb77f7b54
Jorgeiuts <jorge.alonso241099@gmail.com>
2024-02-26 11:05:53 -0600

    Adding function to calculate a Kleene lock

--- src/com/jcastaneda/main/CerraduraKleene.java
+++ src/com/jcastaneda/main/CerraduraKleene.java
@@ -1,7 +1,30 @@
 package com.jcastaneda.main;
 
+import java.util.ArrayList;
+
 public class CerraduraKleene {
     public static void main(String[] args) {
 
+    }
+
+    public static ArrayList<String> cerraduraKleene(ArrayList<String> lenguage,int words) {
+        ArrayList<String> result = new ArrayList<>();
+
+        if(words == 0){
+            result.add("");
+        }
+        else {
+            result.addAll(lenguage);
+            for (int i = 1; i < words; i++){
+                ArrayList<String> temp = new ArrayList<>();
+                for (String word: result) {
+                    for (String originalWord: lenguage) {
+                        temp.add(word + originalWord);
+                    }
+                }
+                result = temp;
+            }
+        }
+        return result;
     }
 }
